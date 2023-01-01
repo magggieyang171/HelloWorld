994f88ff5c84feb0792cc086db555f579f9f5c08
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2022-12-31 16:54:19 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C D E F G A B C5 ", 120)
+})
